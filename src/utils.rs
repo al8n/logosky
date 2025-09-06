d4b86c13e4bfb429b6f323a365c2284d9655711a
al8n <scygliu1@gmail.com>
2025-09-06 23:08:24 +0800

    rename file

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
 pub use human_display::*;
 pub use positioned_char::*;
-pub use unexpected_eot::*;
+pub use unexpected_end::*;
 pub use unexpected_lexeme::*;
 
 mod human_display;
 mod positioned_char;
-mod unexpected_eot;
+mod unexpected_end;
 mod unexpected_lexeme;
