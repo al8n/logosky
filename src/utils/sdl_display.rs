a6a19eae152f7b6506ce4ba4022529ffd04e84c0
al8n <scygliu1@gmail.com>
2025-10-15 18:04:07 +1300

    Re-design SDL display related traits and structures

--- src/utils/sdl_display.rs
+++ src/utils/sdl_display.rs
@@ -1,103 +1,152 @@
+mod sealed {
+  pub trait Sealed {}
+
+  impl<T: ?Sized + Sealed> Sealed for &T {}
+}
+
 /// A trait for displaying in a SDL.
-pub trait DisplaySDL {
+///
+/// This trait is not meant to be implemented outside of this crate.
+/// Implementing [`DisplayCompact`] or [`DisplayPretty`], and use the [`CompactDisplay`] or [`PrettyDisplay`] wrappers,
+/// which implement this trait.
+pub trait DisplaySDL: sealed::Sealed {
+  /// The options for SDL display.
+  type Options: ?Sized;
+
   /// Formats the value in a SDL.
-  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result;
-
-  /// Formats the value in a compact SDL.
-  #[inline(always)]
-  fn fmt_compact(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-    self.fmt(f)
-  }
-
-  /// Formats the value in a pretty SDL.
-  #[inline(always)]
-  fn fmt_pretty(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-    self.fmt(f)
-  }
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result;
 
   /// Returns a wrapper which implement `Display`.
-  #[inline(always)]
-  fn display(&self) -> SDLDisplay<'_, Self> {
-    SDLDisplay::default(self)
-  }
-
-  /// Returns a wrapper which implement `Display` in a compact SDL.
-  #[inline(always)]
-  fn display_compact(&self) -> SDLDisplay<'_, Self> {
-    SDLDisplay::compact(self)
-  }
-
-  /// Returns a wrapper which implement `Display` in a pretty SDL.
-  #[inline(always)]
-  fn display_pretty(&self) -> SDLDisplay<'_, Self> {
-    SDLDisplay::pretty(self)
-  }
+  fn display<'a>(&'a self, options: &'a Self::Options) -> impl core::fmt::Display + 'a;
 }
 
 impl<T: DisplaySDL + ?Sized> DisplaySDL for &T {
+  type Options = T::Options;
+
   #[inline]
-  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-    (*self).fmt(f)
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result {
+    <T as DisplaySDL>::fmt(*self, f, options)
   }
 
-  #[inline(always)]
-  fn fmt_compact(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-    (*self).fmt_compact(f)
-  }
-
-  #[inline(always)]
-  fn fmt_pretty(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-    (*self).fmt_pretty(f)
+  fn display<'a>(&'a self, options: &'a Self::Options) -> impl core::fmt::Display + 'a {
+    <T as DisplaySDL>::display(*self, options)
   }
 }
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-enum DisplayFmt {
-  Compact,
-  Pretty,
-  #[default]
-  Default,
+/// A trait for displaying in a compact way.
+pub trait DisplayCompact {
+  /// The options for compact display.
+  type Options: ?Sized;
+
+  /// Formats the value in a compact way.
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result;
+
+  /// Returns a wrapper which implement `Display` in a compact way.
+  #[inline(always)]
+  fn display<'a>(&'a self, options: &'a Self::Options) -> CompactDisplay<'a, Self> {
+    CompactDisplay { t: self, options }
+  }
 }
 
-/// A helper struct for displaying in a SDL.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct SDLDisplay<'a, T: ?Sized> {
+impl<T: DisplayCompact + ?Sized> DisplayCompact for &T {
+  type Options = T::Options;
+
+  #[inline]
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result {
+    (*self).fmt(f, options)
+  }
+}
+
+/// A trait for displaying in a pretty way.
+pub trait DisplayPretty {
+  /// The options for pretty display.
+  type Options: ?Sized;
+
+  /// Formats the value in a pretty way.
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result;
+
+  /// Returns a wrapper which implement `Display` in a pretty way.
+  #[inline(always)]
+  fn display<'a>(&'a self, options: &'a Self::Options) -> PrettyDisplay<'a, Self> {
+    PrettyDisplay { t: self, options }
+  }
+}
+
+impl<T: DisplayPretty + ?Sized> DisplayPretty for &T {
+  type Options = T::Options;
+
+  #[inline]
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result {
+    (*self).fmt(f, options)
+  }
+}
+
+/// A helper struct for displaying in a compact way.
+pub struct CompactDisplay<'a, T: ?Sized + DisplayCompact> {
   t: &'a T,
-  fmt: DisplayFmt,
+  options: &'a T::Options,
 }
 
-impl<T: DisplaySDL + ?Sized> core::fmt::Display for SDLDisplay<'_, T> {
+impl<T> core::fmt::Display for CompactDisplay<'_, T>
+where
+  T: DisplayCompact + ?Sized,
+{
   #[inline]
   fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-    match self.fmt {
-      DisplayFmt::Compact => self.t.fmt_compact(f),
-      DisplayFmt::Pretty => self.t.fmt_pretty(f),
-      DisplayFmt::Default => self.t.fmt(f),
-    }
+    self.t.fmt(f, self.options)
   }
 }
 
-impl<'a, T: ?Sized> SDLDisplay<'a, T> {
-  /// Formats the value in a compact SDL.
-  #[inline(always)]
-  pub const fn compact(t: &'a T) -> Self {
-    Self::with_fmt(t, DisplayFmt::Compact)
-  }
+impl<T> sealed::Sealed for CompactDisplay<'_, T> where T: DisplayCompact + ?Sized {}
 
-  /// Formats the value in a pretty SDL.
-  #[inline(always)]
-  pub const fn pretty(t: &'a T) -> Self {
-    Self::with_fmt(t, DisplayFmt::Pretty)
-  }
+impl<T> DisplaySDL for CompactDisplay<'_, T>
+where
+  T: DisplayCompact + ?Sized,
+{
+  type Options = T::Options;
 
-  /// Formats the value in a default SDL.
-  #[inline(always)]
-  pub const fn default(t: &'a T) -> Self {
-    Self::with_fmt(t, DisplayFmt::Default)
+  #[inline]
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result {
+    self.t.fmt(f, options)
   }
 
   #[inline(always)]
-  const fn with_fmt(t: &'a T, fmt: DisplayFmt) -> Self {
-    Self { fmt, t }
+  fn display<'a>(&'a self, options: &'a Self::Options) -> impl core::fmt::Display + 'a {
+    self.t.display(options)
+  }
+}
+
+/// A helper struct for displaying in a pretty way.
+pub struct PrettyDisplay<'a, T: ?Sized + DisplayPretty> {
+  t: &'a T,
+  options: &'a T::Options,
+}
+
+impl<T> core::fmt::Display for PrettyDisplay<'_, T>
+where
+  T: DisplayPretty + ?Sized,
+{
+  #[inline]
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    self.t.fmt(f, self.options)
+  }
+}
+
+impl<T> sealed::Sealed for PrettyDisplay<'_, T> where T: DisplayPretty + ?Sized {}
+
+impl<T> DisplaySDL for PrettyDisplay<'_, T>
+where
+  T: DisplayPretty + ?Sized,
+{
+  type Options = T::Options;
+
+  #[inline]
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>, options: &Self::Options) -> core::fmt::Result {
+    self.t.fmt(f, options)
+  }
+
+  #[inline(always)]
+  fn display<'a>(&'a self, options: &'a Self::Options) -> impl core::fmt::Display + 'a {
+    self.t.display(options)
   }
 }
