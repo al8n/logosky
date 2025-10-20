2747a7c617d5d50325dfd47cbe12db91c3e96b19
al8n <scygliu1@gmail.com>
2025-10-20 21:33:23 +1300

    update

--- build.rs
+++ build.rs
@@ -1,20 +1,22 @@
-use std::env;
+use std::env::var;
 
 fn main() {
-  // Avoid unnecessary rebuilds.
+  // Don't rerun this on changes other than build.rs, as we only depend on
+  // the rustc version.
   println!("cargo:rerun-if-changed=build.rs");
 
-  // Always rerun if these env vars change.
-  println!("cargo:rerun-if-env-changed=CARGO_TARPAULIN");
-  println!("cargo:rerun-if-env-changed=CARGO_CFG_TARPAULIN");
+  // Check for `--features=crc32`.
+  let tarpaulin = var("CARGO_FEATURE_TARPAULIN").is_ok();
 
-  // Detect tarpaulin by environment variable
-  if env::var("CARGO_TARPAULIN").is_ok() {
-    enable_cfg("tarpaulin");
+  if tarpaulin {
+    use_feature("tarpaulin");
   }
+
+  // Rerun this script if any of our features or configuration flags change,
+  // or if the toolchain we used for feature detection changes.
+  println!("cargo:rerun-if-env-changed=CARGO_FEATURE_TARPAULIN");
 }
 
-/// Emit `cargo:rustc-cfg=...` to define a conditional compilation flag.
-fn enable_cfg(flag: &str) {
-  println!("cargo:rustc-cfg={}", flag);
+fn use_feature(feature: &str) {
+  println!("cargo:rustc-cfg={}", feature);
 }
